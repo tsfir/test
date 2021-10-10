ffe72462d806bd158fc425b3374f5ab334b57564
Ron <ron.topol@gmail.com>
2021-10-11 00:20:33 +0300

    start2

---
+++ Assets/Follow.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+
+    [SerializeField] GameObject thingToFollow;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = thingToFollow.transform.position + new Vector3(0, 0, -10);
+    }
+}
